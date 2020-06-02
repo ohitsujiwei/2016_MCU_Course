a8123e64959fc0eb880a1f01fe8f73d554c7cbaa
ohitsujiwei <ohitsujiwei@gmail.com>
2020-06-03 05:01:39 +0800

    Update server.c

--- server.c
+++ server.c
@@ -1,8 +1,8 @@
-#include <arpa/inet.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <sys/socket.h>
+#include <arpa/inet.h>
 #include <unistd.h>
 
 int main(int argc, char *argv[]) {
